4584d0d3e17bdca93a3d1be885a9542a67693167
Ajay-sen <ajaysen4736@gmail.com>
2026-03-23 17:14:30 +0530

    Deugging deployment issues

--- client/src/components/AddClientForm.jsx
+++ client/src/components/AddClientForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import API from '../api';
+import API from '../api.js';
 
 const AddClientForm = ({ onClientAdded }) => {
   const [isOpen, setIsOpen] = useState(false);
--- client/src/components/TaskForm.jsx
+++ client/src/components/TaskForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import API from '../api';
+import API from '../api.js';
 
 const TaskForm = ({ clientId, onTaskAdded }) => {
   const [formData, setFormData] = useState({
