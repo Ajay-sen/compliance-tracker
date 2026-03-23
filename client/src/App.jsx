bd3030eb280af2b235e0cf259ea34e5ae0f30e30
Ajay-sen <ajaysen4736@gmail.com>
2026-03-23 15:47:09 +0530

    Manual React and Tailwind configuration

---
+++ client/src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold text-blue-600">Compliance Tracker Setup Success!</h1>
+    </div>
+  )
+}
+export default App
